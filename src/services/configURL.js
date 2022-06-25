e66b27341c0f88abf1c9ff5694a5551e7e4c4f38
nguyenthienan <nguyenthienanz2510@gmail.com>
2022-06-26 00:29:04 +0700

    heroku

--- src/services/configURL.js
+++ src/services/configURL.js
@@ -1,3 +1,5 @@
 export const BASE_URL = "https://movienew.cybersoft.edu.vn";
 export const TOKEN_CYBERSOFT =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAwMyIsIkhldEhhblN0cmluZyI6IjAxLzAxLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY3MjUzMTIwMDAwMCIsIm5iZiI6MTY0NzUzNjQwMCwiZXhwIjoxNjcyNjc4ODAwfQ.v1pky9yKwnujpoxePbaS26rxq_cGpKrk0GvA0sHAVqY";
+
+// test
